bc2b1e7fb71d03030a030595decbed99b56c1a6a
Nitish Kakkar <nitishkakkar@outlook.com>
2017-05-09 01:25:36 +0530

    Auto-Property enhancements

---
+++ CodeSamples/CSharpFeatures/CSharp6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples
+{
+    // This class will demenostrate new features introduced in C# 6.0
+    public class CSharp6
+    {
+        //Auto-Property enhancements
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Auto Property Initialization
+        public string FirstNameInitialization { get; set; } = "Name";
+
+        //Read Only Auto Property
+        public string ReadOnlyFirstName { get; private set; }
+        public string ReadOnlyLastName { get; private set; }
+
+        //True Read-only behavior
+        public string TrueReadOnlyFirstName { get; }
+        public string TrueReadOnlyLastName { get; }
+    }
+}
