3246bddf99332cfa05da59c6d76ec1992b390c5a
Nitish Kakkar <nitishkakkar@outlook.com>
2017-05-09 02:33:50 +0530

    extended function to Write the log. Interpolated Strings

--- CodeSamples/CSharpFeatures/CSharp6.cs
+++ CodeSamples/CSharpFeatures/CSharp6.cs
@@ -23,5 +23,54 @@ namespace CodeSamples
         //True Read-only behavior
         public string TrueReadOnlyFirstName { get; }
         public string TrueReadOnlyLastName { get; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
+        public string Message { get; set; } = "Message";
+
+        /// <summary>
+        /// Adds the specified first.
+        /// </summary>
+        /// <param name="first">The first.</param>
+        /// <param name="second">The second.</param>
+        /// <returns></returns>
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        /// <summary>
+        /// Writes the log.
+        /// </summary>
+        public void WriteLog()
+        {
+            Console.WriteLine(Message);
+        }
+
+        /// <summary>
+        /// Add extended.
+        /// </summary>
+        /// <param name="first">The first.</param>
+        /// <param name="second">The second.</param>
+        /// <returns></returns>
+        public int AddEx(int first, int second) => first + second;
+
+        /// <summary>
+        /// extended function to Write the log.
+        /// </summary>
+        public void WriteLogEx() => Console.WriteLine($"{Message}");
+
+        //Interpolated Strings
+        public void WriteMessage() => Console.WriteLine($"Message is {Message} ");
+
+        public void WriteMessageEx1() => Console.WriteLine($"Message is \"important\" {Message} ");
+        public void WriteMessageEx2() => Console.WriteLine($"Message is {(string.IsNullOrWhiteSpace(Message) ? "empty" : "Not empty")} ");
+
+
+
     }
 }
